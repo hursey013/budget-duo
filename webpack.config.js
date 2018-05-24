911fadb368ab3d28d4a3c76980f785ac6d3baa80
Brian Hurst <brian.hurst@gsa.gov>
2018-05-24 18:56:18 -0400

    Update babel

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,8 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env'],
-          },
+            presets: ['@babel/preset-env']
+          }
         },
       },
       {
