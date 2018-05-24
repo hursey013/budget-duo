edf2cc1512fa539ee18d731d5f78f3e8e19d695b
Brian Hurst <brian.hurst@gsa.gov>
2018-05-24 15:07:43 -0400

    Remove moment.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
-const glob = require('glob-all');
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const PurgecssPlugin = require('purgecss-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
@@ -73,26 +71,6 @@ module.exports = {
     new CleanWebpackPlugin(['dist']),
     new ExtractTextPlugin({
       filename: 'style.[hash].css'
-    }),
-    new PurgecssPlugin({
-      paths: glob.sync([
-        './src/index.html',
-        './src/templates/*.handlebars',
-      ]),
-      extractors: [
-        {
-          extractor: class {
-            static extract(content) {
-              return content.match(/[A-z0-9-:\/]+/g)
-            }
-          },
-          extensions: ['handlebars', 'html']
-        }
-      ],
-      fontFace: true,
-      keyframes: true,
-      whitelist: ['animated', 'expanded', 'fadeIn', 'hidden', 'shake'],
-      whitelistPatterns: [/^firebaseui-/, /^mdl-/, /^split-/]
     }),
     new HtmlWebpackPlugin({
       favicon: './src/images/favicon.ico',
