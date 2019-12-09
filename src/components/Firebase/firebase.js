07142031aff7b72b15d17b3eae5076d7c4dee8a1
Brian Hurst <hursey013@protonmail.com>
2019-12-08 21:08:32 -0500

    Add measurementId to firebase config

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -9,7 +9,8 @@ const config = {
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 class Firebase {
